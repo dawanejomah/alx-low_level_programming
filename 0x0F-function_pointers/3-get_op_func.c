659f595e67a936d08f0c9ea73659e9dcc7f02283
Dawan Ejomah <dawanejomah@gmail.com>
2023-08-16 16:13:24 -0700

    Task 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,52 +1,33 @@
-#include <stdlib.h>
 #include "3-calc.h"
+
 /**
-*op_add - add two numbers
-*@a: number a
-*@b: number b
-*Return: a + b
-*/
-int op_add(int a, int b)
+ * get_op_func - function pointer that selects the correct function to perform
+ * the operation asked by the user
+ * @s: the operator given by the user
+ *
+ * Return: pointer to the function that corresponds to the
+ * operator given as a parameter
+ */
+int (*get_op_func(char *s))(int, int)
 {
-	return (a + b);
-}
-/**
-*op_sub - subtrates two numbers
-*@a: number a
-*@b: number b
-*Return: a - b
-*/
-int op_sub(int a, int b)
-{
-	return (a - b);
-}
-/**
-*op_mul - multiply two numbers
-*@a: number a
-*@b: number b
-*Return: a * b
-*/
-int op_mul(int a, int b)
-{
-	return (a * b);
-}
-/**
-*op_div - divide two numbers
-*@a: number a
-*@b: number b
-*Return: a / b
-*/
-int op_div(int a, int b)
-{
-	return (a / b);
-}
-/**
-*op_mod - module of two numbers
-*@a: number a
-*@b: number b
-*Return: a % b
-*/
-int op_mod(int a, int b)
-{
-	return (a % b);
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+
+	while (ops[i].op)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+			return (ops[i].f);
+		i++;
+	}
+
+	return (NULL);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,39 +1,46 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "3-calc.h"
-/**
-*main - performs simple operations.
-*@argv: array of string
-*@argc: number of arguments
-*Return: 0
-*/
 
+/**
+ * main - program that perfroms simple operations
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
-	int n1, n2, result;
-	int (*op_func)(int, int);
-	char *operator;
+	int arg1, arg2, result;
+	char o;
+	int (*func)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	operator = argv[2];
-	n1 = atoi(argv[1]);
-	n2 = atoi(argv[3]);
-	if ((*operator == '/' || *operator == '%') && n2 == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-	op_func = get_op_func(operator);
-	if (op_func == NULL)
+
+	arg1 = atoi(argv[1]);
+	arg2 = atoi(argv[3]);
+
+	func = get_op_func(argv[2]);
+
+	if (!func)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	result = op_func(n1, n2);
+
+	o = *argv[2];
+
+	if ((o == '/' || o == '%') && arg2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = func(arg1, arg2);
+
 	printf("%d\n", result);
+
 	return (0);
 }
