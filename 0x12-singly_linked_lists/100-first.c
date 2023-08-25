31927fb18bdb76b68fd59fb21cc155e5ac1fc253
Dawan Ejomah <dawanejomah@gmail.com>
2023-08-24 18:38:47 -0700

    100-first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * tortoise -  print before mian.
+ *
+ * Return: nothing
+ */
+void tortoise(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
